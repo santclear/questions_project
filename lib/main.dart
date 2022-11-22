2dc8a788ab6d63b86951f5b51b8165017198c25d
Sant'Clear <santclear@gmail.com>
2022-11-22 20:58:23 -0300

    06 - Main method with arrow function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new QuestionsApp());
-}
+main() => runApp(new QuestionsApp());
 
 class QuestionsApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello Flutter!!'),
