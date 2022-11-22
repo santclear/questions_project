1d8b16c3dfd193156a9d21f6366477a28aa7496b
Sant'Clear <santclear@gmail.com>
2022-11-22 20:57:09 -0300

    05 - First app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(new QuestionsApp());
+}
+
+class QuestionsApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello Flutter!!'),
+    );
+  }
+}
