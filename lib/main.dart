edbffb1d981fc9f25df4867e9c2dbc6a24f81a7d
Sant'Clear <santclear@gmail.com>
2022-11-22 21:04:05 -0300

    08 - Add ElevatedButton.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 main() => runApp(new QuestionsApp());
 
 class QuestionsApp extends StatelessWidget {
+  final questions = [
+    'What is your favorite color?',
+    'What is your favorite pet?'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +15,23 @@ class QuestionsApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Questions'),
         ),
-        body: Text('Hello Flutter!'),
+        body: Column(
+          children: [
+            Text(questions[0]),
+            ElevatedButton(
+              child: Text('Question 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Question 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Question 3'),
+              onPressed: null,
+            )
+          ],
+        ),
       ),
     );
   }
