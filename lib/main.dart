d03648d6cf88ee15ce388d1a684f53d51efc7b3f
Sant'Clear <santclear@gmail.com>
2022-11-22 21:06:44 -0300

    09 - Created functions and call by buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(new QuestionsApp());
+main() => runApp(QuestionsApp());
 
 class QuestionsApp extends StatelessWidget {
   final questions = [
@@ -8,9 +8,22 @@ class QuestionsApp extends StatelessWidget {
     'What is your favorite pet?'
   ];
 
+  void answer() {
+    print('Ask answered');
+  }
+
+  void Function() functionThatReturnAnotherFunction() {
+    return () {
+      print('Ask answered #02!');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          materialTapTargetSize: MaterialTapTargetSize.padded),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Questions'),
@@ -19,16 +32,23 @@ class QuestionsApp extends StatelessWidget {
           children: [
             Text(questions[0]),
             ElevatedButton(
+              //style: ElevatedButton.styleFrom(padding: EdgeInsets.all(50)),
               child: Text('Question 1'),
-              onPressed: null,
+              onPressed: answer,
             ),
             ElevatedButton(
               child: Text('Question 2'),
-              onPressed: null,
+              onPressed: () {
+                print('The ask 2 is selected');
+              },
             ),
             ElevatedButton(
-              child: Text('Question 3'),
-              onPressed: null,
+              child: const Text('Question 3'),
+              onPressed: () => print('The question 3 is selected'),
+            ),
+            ElevatedButton(
+              child: Text('Question 4'),
+              onPressed: functionThatReturnAnotherFunction(),
             )
           ],
         ),
