eda5b064bec4e9a24e0e0fba74283a58550bfe16
Sant'Clear <santclear@gmail.com>
2022-11-24 12:43:37 -0300

    17 - Changed questions var from list to map type.

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,26 @@ main() => runApp(QuestionsApp());
 class _QuestionsAppState extends State<QuestionsApp> {
   var _questionSelected = 0;
 
-  final questions = [
-    'What is your favorite color?',
-    'What is your favorite pet?'
+  final List<Map<String, Object>> questions = [
+    {
+      'text': 'What is your favorite color?',
+      'answers': ['Black','Red','Green','White']
+    },
+    {
+      'text': 'What is your favorite pet?',
+      'answers': ['Rabbit','Snake','Elephant','Lion']
+    },
+    {
+      'text': 'What is your favorite instructor?',
+      'answers': ['Maria','João','Leo','Pedro']
+    }
   ];
 
   void _answer() {
     setState(() {
       _questionSelected++;
     });
+    print(_questionSelected);
   }
 
   void Function() functionThatReturnAnotherFunction() {
@@ -36,7 +47,7 @@ class _QuestionsAppState extends State<QuestionsApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionSelected]),
+            Question(questions[_questionSelected]['text'].toString()),
             Answer('Answer 1', _answer),
             Answer('Answer 2', _answer),
             Answer('Answer 3', _answer)
